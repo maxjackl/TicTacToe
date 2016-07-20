66417de248d077d7bc00fc35973e8c75a335da0d
you@example.com <you@example.com>
2016-07-19 23:49:52 -0400

    dividing projects and work on TTT

---
+++ ticTacToe.js
@@ -0,0 +1,10 @@
+
+function changeText(){
+	document.getElementById('9').innerHTML = 'Fred Flinstone';
+}
+
+function check(place){
+	document.getElementById(place).innerHTML = 'X';
+}
+
+alert("works");
