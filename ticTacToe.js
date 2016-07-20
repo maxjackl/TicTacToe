8a4ad435f740390fe0aeab92ef31d90701b20958
you@example.com <you@example.com>
2016-07-20 18:32:24 -0400

    after adding a lof of functionality to Tic Tac Toe

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,10 +1,68 @@
-
-function changeText(){
-	document.getElementById('9').innerHTML = 'Fred Flinstone';
-}
+var arrayX = [];
+var arrayY = [];
+var turn = 'X';
 
 function check(place){
-	document.getElementById(place).innerHTML = 'X';
+	if (checkEmpty(place)) {
+		if (turn === 'X') {
+			arrayX.push(place)
+			drawScreen(place, 'X');
+			checkWinningCondition('X');
+			turn = 'O';
+		}
+		else {
+			arrayY.push(place)
+			drawScreen(place, 'O');
+			turn = 'X';
+			checkWinningCondition('Y');
+		}
+	}
 }
 
-alert("works");
+function drawScreen(place, player) {
+	document.getElementById(place).innerHTML = player;
+}
+
+function checkEmpty(place) {
+	if (arrayX.indexOf(place) == -1 && arrayY.indexOf(place) == -1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function checkWinningCondition(player) {
+	var winningArray = [
+		[1,2,3],
+		[4,5,6]
+	];
+
+	console.log(JSON.stringify(arrayX));
+	console.log(JSON.stringify(winningArray[i]));
+
+	for (var i=0; i < winningArray.length; i++) {
+		if (JSON.stringify(arrayX.sort()) == JSON.stringify(winningArray[i])) {
+			console.log(JSON.stringify(arrayX));
+			console.log(JSON.stringify(winningArray[i]));
+			alert("game over. X won");
+			return true;
+		}
+
+}
+}
+
+
+
+/*
+
+
+
+
+*/
+
+		/*
+		for (var k=0; i < winningArray[i]; k++) {
+
+		}
+		*/
