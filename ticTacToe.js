a90a0be7325d59595556c9c756c2b06fbb323d2b
you@example.com <you@example.com>
2016-07-29 17:03:36 -0400

    tried to add random move but failed as of now

--- ticTacToe.js
+++ ticTacToe.js
@@ -127,6 +127,14 @@ var INTLOGIC = (function() {
 			return arrayO;
 		}
 
+		function returnTurn() {
+			return turn;
+		}
+
+		function returnPlayerWon() {
+			return playerWon;
+		}
+
 
 	return {
 		check : check,
@@ -136,7 +144,9 @@ var INTLOGIC = (function() {
 		returnArrayX : returnArrayX,
 		returnArrayO : returnArrayO,
 		checkEmpty : checkEmpty,
-		checkWinningCondition : checkWinningCondition
+		checkWinningCondition : checkWinningCondition,
+		returnTurn : returnTurn,
+		returnPlayerWon : returnPlayerWon
 	}
 
 })();
@@ -201,34 +211,49 @@ var AI = function() {
 
 	function makeMove() {
 
-		if (INTLOGIC.returnTurnCountO() >= 2) {
-			checkWinNextMove(INTLOGIC.returnArrayO());
-		}
-
-		if (INTLOGIC.returnTurnCountX() >= 2) {
-			checkLoseNextMove(INTLOGIC.returnArrayX());
-		}
-
-
-		console.log("Time to make a move");
-
-		console.log(INTLOGIC.returnTurnCountO() + INTLOGIC.returnArrayX())
-
 		//If X makes the first move in the middle
 		if (INTLOGIC.returnTurnCountO() === 0
 				&& JSON.stringify(INTLOGIC.returnArrayX()) === JSON.stringify([5])) {
 
-
-
 			INTLOGIC.check(1);
-console.log("Time to make a specific move");
+			console.log("Time to move to the 1 position");
+			return;
 		}
 
 		//If X makes the second move in the 9th position
-		if (INTLOGIC.returnTurnCountO() === 1
+		else if (INTLOGIC.returnTurnCountO() === 1
 				&& JSON.stringify(INTLOGIC.returnArrayX()) === JSON.stringify([5, 9])) {
 					INTLOGIC.check(7);
-	}
+					console.log("checking for the X move to 5 and 9");
+					return;
+		}
+
+		if (INTLOGIC.returnTurnCountO() >= 2) {
+			checkWinNextMove(INTLOGIC.returnArrayO());
+			console.log("checking for the winning move");
+		}
+
+		if (INTLOGIC.returnTurnCountX() >= 2) {
+			checkLoseNextMove(INTLOGIC.returnArrayX());
+			console.log("checking for the loosing move");
+		}
+
+
+		//console.log("Time to make a move");
+
+		//console.log(INTLOGIC.returnTurnCountO() + INTLOGIC.returnArrayX())
+
+
+
+
+		console.log('checking for a random number');
+		makeRandomMove();
+
+
+
+
+
+
 
 }
 
@@ -266,6 +291,32 @@ console.log("Time to make a specific move");
 		}
 	}
 
+	function randomPosition() {
+		var randomPostion = Math.floor(Math.random() * 10);
+
+		while (randomPostion === 0) {
+			randomPostion = Math.floor(Math.random() * 10);
+		}
+
+		return randomPostion;
+	}
+
+	function makeRandomMove() {
+
+		console.log("making a randoom move");
+		var randomMoveTemp = randomPosition();
+		while (!INTLOGIC.checkEmpty(randomMoveTemp) || !INTLOGIC.returnPlayerWon()) {
+			console.log(randomMoveTemp);
+			randomMoveTemp = randomPosition();
+			console.log(randomMoveTemp);
+		}
+
+		if (INTLOGIC.returnTurn === '0') {
+			INTLOGIC.check(randomMoveTemp);
+			return;
+		}
+
+	}
 
 	return {
 		makeMove : makeMove
